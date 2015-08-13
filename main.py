ec8cc89d94f0a17ee3eaf2036c5fe839c9c4fcd8
Jonas <shaitan25@gmail.com>
2015-08-13 18:20:29 +0200

    Initial creation of main and gitignore

---
+++ main.py
@@ -0,0 +1,18 @@
+#######################################
+# 
+#   League of Item Sets
+#
+#   Version: 0.0
+#
+#   By: Jonas Sandbekk & Ole Harbosen
+# 
+#   In collaberation with Riot Games
+#
+#   For the Riot API Challenge 2.0
+#
+#######################################
+
+
+if __name__ == "__main__":
+
+    print("Main works! Hello world!")
