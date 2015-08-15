996150aa28d0d4c7e63b7cdaca72cb7f706d5dca
Jonas <shaitan25@gmail.com>
2015-08-15 16:23:09 +0200

    Checking for version (temporary solution), lots of updates to the methods inside update.py so that they are general and not spesific to items: they have been converted item -> json. Also downloaded all champion icons to make sure it worked.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import update
 import logging as log
 import re
+import urllib.request
+import json
 
 #######################################
 # 
@@ -18,6 +20,50 @@ import re
 #
 #######################################
 
+def check_version(region, api_key, local_version):
+    '''
+Check current version of the region.
+
+Parameters
+-------------
+region : str
+    Region to be examined
+api_key : str
+    Api_key to use in request
+local_version : str
+    Local version for region in format 5.15.1
+
+Returns
+-------------
+new_version : bool
+    True for new version, False else
+current_version : str
+    The current version for the region
+    in the format 5.15.1
+    '''
+    log.debug("Checking version for "+region)
+    
+    url = "https://global.api.pvp.net/api/lol/static-data/" + region + \
+          "/v1.2/versions?api_key=" + api_key
+    
+    try:
+        with urllib.request.urlopen(url) as response:
+            net_version = response.read().decode("UTF-8")
+    except urllib.error.HTTPError:
+        error("HTTPError when version checking for " + region)
+        return False, current_version
+
+    net_version = json.loads(net_version)[0]
+    
+    if (local_version == net_version):
+        current_version = local_version
+        is_new_version = False
+    else:
+        current_version = net_version
+        is_new_version = True
+
+    return is_new_version, current_version
+
 
 if __name__ == "__main__":
   
@@ -27,9 +73,35 @@ if __name__ == "__main__":
     print("League of Item Sets v0.1")
     
     # Get the api_key, which is hidden for git purposes
-    with open("settings") as api_key_file:
-        api_key_string = api_key_file.read()
-        api_key = re.search(r'API_KEY:\s*([?\w\d\-]+)\s', api_key_string).group(1)
+    try:
+        with open("settings") as settings_file:
+            settings_string = settings_file.read()
+            api_key = re.search(r'API_KEY:\s*([?\w\d\-]+)\s', settings_string)
 
-    # Update all the json file from all the different regions
-    update.get_all_items(api_key, "DEBUG")
+            if (api_key is None):
+                log.error("""Could not locate api_key inside settings file.
+                Please input an api_key in the settings file in the format: 
+                API_KEY: <API_KEY>""")
+            else:
+                api_key = api_key.group(1)
+
+            # This is temporary. Will be exchanged once we have a db with a table
+            # which contains all versions for all regions
+            current_version = re.search(r'CURRENT_VERSION: ([?\d\.]+\s)', settings_string)
+
+            if (current_version is None):
+                log.error("""Could not locate the current version inside the settings file.
+                Please input the current version inside the settings file in format:
+                CURRENT_VERSION: <VERSION>""")
+            else:
+                current_version = current_version.group(1)
+
+            # Temporary just use eune
+            if (check_version( "eune", api_key, current_version )):
+                log.debug("We are up to date!")
+            else:
+                # Update all the json file from all the different regions
+                update.update_all(api_key, current_version, "DEBUG")
+                
+    except FileNotFoundError:
+        log.error("Settings file not found. Please create a settings file.")
--- update.py
+++ update.py
@@ -9,51 +9,6 @@ def error(msg):
     log.error(msg)
 
     
-def check_version(region, api_key, local_version):
-    '''
-Check current version of the region.
-
-Parameters
--------------
-region : str
-    Region to be examined
-api_key : str
-    Api_key to use in request
-local_version : str
-    Local version for region in format 5.15.1
-
-Returns
--------------
-new_version : bool
-    True for new version, False else
-current_version : str
-    The current version for the region
-    in the format 5.15.1
-    '''
-    log.debug("Checking version for "+region)
-    
-    url = "https://global.api.pvp.net/api/lol/static-data/" + region + \
-          "/v1.2/versions?api_key=" + api_key
-    
-    try:
-        with urllib.request.urlopen(url) as response:
-            net_version = response.read()
-    except urllib.error.HTTPError:
-        error("HTTPError when version checking for " + region)
-        return False, current_version
-
-    net_version = json.loads(net_version)[0]
-    
-    if (local_version == net_version):
-        current_version = local_version
-        is_new_version = False
-    else:
-        current_version = net_version
-        is_new_version = True
-
-    return is_new_version, current_version
-
-
 def get_icons(img_type, id_list, version, overwrite=False):
     '''
 Download all icons.
@@ -92,17 +47,19 @@ overwrite : bool
     log.debug("Done fetching all icons")
 
     
-def get_items(region, api_key):  
+def get_json(json_type, url, region):  
     '''
-Fetch json file with items.
+Fetch json file.
 
 Parameters
 -------------
+json_type : str
+    What type of json we want to fetch (ex. champion or item)
+url : str
+    URL to fetch json from
 region : str
     Region code for which region we
-    want to download from.
-api_key : str
-    Api key to use for request
+    want to fetch from.
 
 Returns
 -------------
@@ -110,12 +67,9 @@ html : str
     This is a json file with all
     the items for the given region.
     '''
-    items_id_list = []
-    log.debug("Fetching items for " + region)
+    icon_id_list = []
+    log.debug("Fetching " + json_type + " for " + region)
     
-    url = "https://global.api.pvp.net/api/lol/static-data/" + region + \
-          "/v1.2/item?itemListData=all&api_key=" + api_key
-
     try:
         with urllib.request.urlopen(url) as response:
             html = response.read().decode("UTF-8")
@@ -128,23 +82,23 @@ html : str
     if ("na" in region):
         html_json = json.loads( html )
 
-        for image_id in html_json["data"]:
-            items_id_list.append(image_id)
+        for icon_id in html_json["data"]:
+            icon_id_list.append(icon_id)
 
-        get_icons("item", items_id_list, "5.15.1")
+        get_icons(json_type, icon_id_list, "5.15.1")
                              
     log.debug("Succesfully fetched items for " + region)
     
     return html
 
     
-def check_items_version(net_items_string, region):
+def check_json_version(json_type, net_json_string, region):
     '''
 Compare local version of file to the one on the net.
 
 Parameters
 -------------
-net_items_string : str
+net_json_string : str
     String version of JSON file we
     fetched via the API.
 region : str
@@ -160,19 +114,19 @@ is_new_version : bool
     
     # If we can't find the file, we want to save it
     try:
-        local_items_file = open("items_json/" + region,'r')
+        local_json_file = open("json/" + json_type + "/" + region,'r')
     except FileNotFoundError:
         return True
     
-    local_items_json = json.load(local_items_file)
+    local_json_version = json.load(local_json_file)
 
-    log.debug("Local version: " + local_items_json["version"])
+    log.debug("Local version: " + local_json_version["version"])
 
-    net_items_json = json.loads(net_items_string)
+    net_json_version = json.loads(net_json_string)
 
-    log.debug("Net version: " + net_items_json["version"])
+    log.debug("Net version: " + net_json_version["version"])
 
-    if (local_items_json["version"] == net_items_json["version"]):
+    if (local_json_version["version"] == net_json_version["version"]):
         is_new_version = False
     else:
         is_new_version = True
@@ -180,35 +134,45 @@ is_new_version : bool
     return is_new_version
 
 
-def get_all_items(api_key, loglvl):
+def get_all_json(url_list, region_list):
     '''
-Fetch all the items! (as json files)
+Fetch all the json.
 
 Parameters
 -------------
-api_key : str
-    Api key to use for requests
-loglvl : str
-    At which level we should print logging
+url_list : list[str]
+    List of all the different types we want
+    and url that comes we fetch from.
+region_list : list[str]
+    List of all the different regions we fetch
     '''
-    #region_list = ["br","eune","euw","kr","lan","las","na","oce","ru","tr","pbe"]
-    region_list = ["na"]
+    log.debug("Beginning fetching for all json files for all regions")
+        
+    for json_type in url_list:
+        for region in region_list:
+            
+            url = url_list[json_type]
+            json_string = get_json(json_type, url, region)
+            
+            if (json_string is None):
+                log.error("Error occured when trying to fetch items for " + region)
+                continue
+            else:
+                if (check_json_version( json_type, json_string, region )):
+                    with open("json/" + json_type + "/" + region, 'w') as json_file:
+                        json_file.write( json_string )
+                else:
+                    log.debug(region + " skipped because it was up to date.")
+                    
+    log.debug("Fetched all items as json files for all regions")
+
     
+def update_all(api_key, current_version, loglvl):
     log.basicConfig(format="%(levelname)s: %(message)s", level=loglvl)
     
-    log.debug("Beginning fetching for all items as json files for all regions")
-        
-    for region in region_list:   
-        items_string = get_items( region, api_key )
-
-        if (items_string is None):
-            log.error("Error occured when trying to fetch items for " + region)
-            continue
-        
-        if (check_items_version( items_string, region )):
-            with open("json/items/" + region, 'w') as items_file:
-                items_file.write( items_string )
-        else:
-            log.debug(region + " skipped because it was up to date.")
-       
-    log.debug("Fetched all items as json files for all regions")
+    #region_list = ["br","eune","euw","kr","lan","las","na","oce","ru","tr","pbe"]
+    region_list = ["na"]
+    url_list = {"item":"https://global.api.pvp.net/api/lol/static-data/eune/v1.2/item?itemListData=all&api_key="+api_key,
+                "champion":"https://global.api.pvp.net/api/lol/static-data/eune/v1.2/champion?champData=all&api_key="+api_key}
+    
+    get_all_json(url_list, region_list)
