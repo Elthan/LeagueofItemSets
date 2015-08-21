37e999619da5b5f728213da2980222f6608f6a2d
Jonas <Elthan@users.noreply.github.com>
2015-08-21 08:56:48 +0200

    Better naming in PlayerItemSet

--- database/models.py
+++ database/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 
 
 class PlayerItemSet(models.Model):
-        ItemSetID = models.CharField(primary_key=True, max_length=50, default="0") #or other field
+        ID = models.CharField(primary_key=True, max_length=50, default="0") #or other field
         Title = models.CharField(max_length=200, default="Made by LoIS")
-        ItemSetDate = models.DateTimeField('date created', default=timezone.now)
-        ItemSetType = models.CharField(max_length=6, default="custom")
+        Date = models.DateTimeField('date created', default=timezone.now)
+        Type = models.CharField(max_length=6, default="custom")
         Map = models.CharField(max_length=3, default="any")
         Mode = models.CharField(max_length=7, default="any")
         Priority = models.BooleanField(default="false")
