5d9f5f053fdc3848b4bf6d0818332b90a1a38c56
Jonas <shaitan25@gmail.com>
2015-08-21 17:52:58 +0200

    Removed unnecessary code.

--- database/views.py
+++ database/views.py
@@ -17,12 +17,6 @@ def send_json_file (emptyarg, file_path="database", filename="test.json"):
     response = HttpResponse(json_file, content_type ='application/json')
     response["Content-Disposition"] = 'attachment; filename="' + filename + '"'
 
-    '''
-    json_file = open(file_path, 'rb')# Select file
-    wrapper = FileWrapper(file(json_file, 'rb'))
-    response = HttpResponse(wrapper, content_type='application/json')
-    response["Content-Length"] = os.path.getsize(json_file)
-    '''
     return response
 
 def rend(request):
