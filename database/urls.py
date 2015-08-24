06227af90a8d7244cc73eb65f79d534ea62cc905
Jonas <Elthan@users.noreply.github.com>
2015-08-24 14:34:21 +0200

    Fix'd items regex

--- database/urls.py
+++ database/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^champion', views.champ_select, name='champion'),
-    url(r'^items/(?[A-Za-z\']+)$', views.item_select, name='items'),
+    url(r'^items/([A-Za-z\']+)/*', views.item_select, name='items'),
     url(r'^$', views.index, name='index'),
     url(r'^sendJSON$', views.send_json_file, name='Send JSON')
 ]
