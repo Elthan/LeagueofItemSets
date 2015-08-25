83b8de65bd7f78c7e854ac51751f3cac3ab693f9
Jonas <Elthan@users.noreply.github.com>
2015-08-25 11:58:08 +0200

    Changed url for items

--- database/urls.py
+++ database/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^champion', views.champ_select, name='champion'),
-    url(r'^items\&champion=([\w]+)/*$', views.item_select, name='items'),
+    url(r'^?=([\w]+)/*$', views.item_select, name='items'),
     url(r'^$', views.index, name='index'),
     url(r'^sendJSON$', views.send_json_file, name='Send JSON')
 ]
